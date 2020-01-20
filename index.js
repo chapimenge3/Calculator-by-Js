6dff1e2563cb2b55ce38fbb67a7a482217633f39
chapimenge3 <chapimenge3@gmail.com>
2020-01-20 16:05:10 +0300

    add event to number keys

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previosOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+
+numberButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
